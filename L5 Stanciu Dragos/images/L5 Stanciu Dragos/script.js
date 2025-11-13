9254e42685fc3954c62f4c67872ab3c535dd5d8c
UltraPipacio <stanciudragos2006@yahoo.ro>
2025-11-13 17:28:44 +0200

    Add files via upload

---
+++ L5 Stanciu Dragos/images/L5 Stanciu Dragos/script.js
@@ -0,0 +1,32 @@
+// Cache the DOM
+var button = document.getElementById("button")
+var nume = document.getElementById("nume")
+var prenume = document.getElementById("prenume")
+var educatie = document.getElementById("educatie")
+var asteptari = document.getElementById("asteptari")
+var img = document.querySelector("body")
+
+// Add event listener
+button.addEventListener("click", AltaViata)
+
+// Define function
+function AltaViata() {
+                        nume.innerHTML = "SuperMegaPipacio"
+                        prenume.innerHTML = "Ciugud SpeedStars"
+
+                        educatie.innerHTML = "Experiente"
+                        asteptari.innerHTML = "<ul><li>
+Zburat cu Skoda in Giratoriu </li><li>Zburat cu BMW in casa parorhiala</li></ul>
+
+                        img.src = "C:\Users\DragosGabrielStanciu\Desktop\Portofoliu-Stanciu-Dragos\L5 Stanciu Dragos\images\smoking-caterpillar-goofy-ahh-desktop-wallpaper-cover
+                        img.style.opacity = "70%"
+                        img.style
+                        img.style
+                        img.style
+                        img.style
+
+
+
+
+
+}
